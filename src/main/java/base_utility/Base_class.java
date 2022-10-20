a9392dcd130ce118192c13804d260975af3ed586
Pratik <Pratik@LAPTOP-949G3N81>
2022-10-20 15:19:24 +0530

    driver

--- src/main/java/base_utility/Base_class.java
+++ src/main/java/base_utility/Base_class.java
@@ -2,9 +2,8 @@ package base_utility;
 
 public class Base_class {
 
-	public static void demo() {
-		System.out.println("Demo class");
-		System.out.println("Hii Deepak");
+	public static void Initialize_object() {
+		System.out.println("objects");
 	}
 	
 }

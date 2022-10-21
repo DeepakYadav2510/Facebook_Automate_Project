6d8a2b77b13ada300104006f05e40b4b56318e62
Pratik <Pratik@LAPTOP-949G3N81>
2022-10-21 15:27:19 +0530

    done

--- src/main/java/pom_package/Pom_class_fb.java
+++ src/main/java/pom_package/Pom_class_fb.java
@@ -16,11 +16,15 @@ public class Pom_class_fb {
 
 	@FindBy(xpath="//input[@type='password']")private WebElement Password_Input;
 
-	@FindBy(xpath="//button[@type='submit']")private WebElement LogIn_Button_Input;
+	@FindBy(xpath="//button[@name='login']")private WebElement LogIn_Button_Input;
 
-	@FindBy(xpath="	//a[text()='Forgotten account?']")private WebElement Forgot_password;
+	@FindBy(xpath="//a[text()='Forgotten password?']")private WebElement Forgot_password;
 	
-	@FindBy(xpath="	//a[@rel='nofollow']")private WebElement SignUp_For_FB;
+	@FindBy(xpath="//h2[text()='Facebook helps you connect and share with the people in your life.']")private WebElement FB_Welcome_Text;
+
+	@FindBy(xpath="//a[text()='Create New Account']")private WebElement Create_New_Account;
+
+
 
 	public void Enter_Email(String email) {
 		Email_Input.sendKeys(email);
@@ -38,8 +42,13 @@ public class Pom_class_fb {
 		 Forgot_password.click(); 
 	}
 	
-	public void Click_on_signUp() {
-		SignUp_For_FB.click();
+	public String Get_welcome_text() {
+		return FB_Welcome_Text.getText();
+		
+	}
+	
+	public void Click_On_Create_Account() {
+		Create_New_Account.click();
 	}
 	
 	
